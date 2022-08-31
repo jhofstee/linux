2ca2f284595840fcf09bf3b3fcab812b077a1ca3
Jeroen Hofstee <jhofstee@victronenergy.com>
2022-08-31 08:51:33 +0200

    can: gs_usb: count state change only once

--- drivers/net/can/usb/gs_usb.c
+++ drivers/net/can/usb/gs_usb.c
@@ -354,26 +354,35 @@ static int gs_cmd_reset(struct gs_can *gsdev)
 static void gs_update_state(struct gs_can *dev, struct can_frame *cf)
 {
 	struct can_device_stats *can_stats = &dev->can.can_stats;
+	enum can_state new_state;
 
 	if (cf->can_id & CAN_ERR_RESTARTED) {
 		dev->can.state = CAN_STATE_ERROR_ACTIVE;
 		can_stats->restarts++;
-	} else if (cf->can_id & CAN_ERR_BUSOFF) {
-		dev->can.state = CAN_STATE_BUS_OFF;
-		can_stats->bus_off++;
+		return;
+	}
+
+	if (cf->can_id & CAN_ERR_BUSOFF) {
+		new_state = CAN_STATE_BUS_OFF;
 	} else if (cf->can_id & CAN_ERR_CRTL) {
 		if ((cf->data[1] & CAN_ERR_CRTL_TX_WARNING) ||
-		    (cf->data[1] & CAN_ERR_CRTL_RX_WARNING)) {
-			dev->can.state = CAN_STATE_ERROR_WARNING;
-			can_stats->error_warning++;
-		} else if ((cf->data[1] & CAN_ERR_CRTL_TX_PASSIVE) ||
-			   (cf->data[1] & CAN_ERR_CRTL_RX_PASSIVE)) {
-			dev->can.state = CAN_STATE_ERROR_PASSIVE;
-			can_stats->error_passive++;
-		} else {
-			dev->can.state = CAN_STATE_ERROR_ACTIVE;
-		}
+		    (cf->data[1] & CAN_ERR_CRTL_RX_WARNING))
+		    new_state = CAN_STATE_ERROR_WARNING;
+		else if ((cf->data[1] & CAN_ERR_CRTL_TX_PASSIVE) ||
+			   (cf->data[1] & CAN_ERR_CRTL_RX_PASSIVE))
+			new_state = CAN_STATE_ERROR_PASSIVE;
+		else
+			new_state = CAN_STATE_ERROR_ACTIVE;
+	} else {
+		return;
 	}
+
+	if (new_state == dev->can.state)
+		return;
+
+	can_update_state_error_stats(dev->netdev, new_state);
+	
+	dev->can.state = new_state;
 }
 
 static void gs_usb_receive_bulk_callback(struct urb *urb)
